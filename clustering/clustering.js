8167bae084cd774c9d356ced633466a342196b4a
JordanTishler <114168663+JordanTishler@users.noreply.github.com>
2023-01-13 16:40:22 -0500

    Update clustering.js

--- clustering/clustering.js
+++ clustering/clustering.js
@@ -1,7 +1,7 @@
 function initMap() {
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 3,
-    center: { lat:46.012567, lng: -81.401889 },
+    center: { lat:46.012567, lng: -81.401889 }
   });
   // Create an array of alphabetical characters used to label the markers.
   const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -136,5 +136,5 @@ const locations = [
 { lat:42.090666, lng: -82.442196 },
 { lat:48.713534, lng: -85.649972 },
 { lat:46.622078, lng: -81.455217 },
-{ lat:51.104, lng: -94.288 },
+{ lat:51.104, lng: -94.288 }
 ];
